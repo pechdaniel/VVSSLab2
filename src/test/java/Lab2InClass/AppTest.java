715814e1237c1f2b5e77313272dc2be3e524d6de
pechd <pech.daniel1999@yahoo.com>
2020-04-01 11:33:44 +0300

    Lab 3

--- src/test/java/Lab2InClass/AppTest.java
+++ src/test/java/Lab2InClass/AppTest.java
@@ -1,6 +1,7 @@
 package Lab2InClass;
 
 import domain.Student;
+import domain.Tema;
 import org.junit.Test;
 import repository.NotaXMLRepo;
 import repository.StudentFileRepository;
@@ -86,5 +87,63 @@ public class AppTest
         }
 
         assertTrue(hasError);
+    }
+
+    private void removeAllTeme(){
+        List<Tema> lst = new ArrayList<Tema>((Collection<? extends Tema>) service.getAllTeme());
+        lst.forEach(s->service.deleteTema(s.getID()));
+    }
+
+    @Test
+    public void TestConditionsAddAssignmentThrowsException(){
+        CreateService();
+        List<Tema> teme = new ArrayList<>();
+        teme=addTemeErrorToList(teme);
+
+        for (Tema t:teme){
+            assertFalse(TestAddAssignment(t));
+        }
+    }
+
+    @Test
+    public void TestConditionsAddAssignmentWorks(){
+        CreateService();
+        List<Tema> teme = new ArrayList<>();
+        teme=addTemeGoodToList(teme);
+
+        for (Tema t:teme){
+            assertTrue(TestAddAssignment(t));
+        }
+    }
+
+    private List<Tema> addTemeErrorToList(List<Tema> teme){
+        teme.add(new Tema("","Desc",12,13));//empty tema id
+        teme.add(new Tema("10","",12,13));//empty description
+        teme.add(new Tema("10","Desc",0,13));//deadline < 1
+        teme.add(new Tema("10","Desc",12,15));//deadline >14
+        teme.add(new Tema("10","Desc",0,13));//rimire < 1
+        teme.add(new Tema("10","Desc",12,15));//primire >14
+
+        return teme;
+    }
+
+    private List<Tema> addTemeGoodToList(List<Tema> teme){
+        teme.add(new Tema("10","Desc",12,13));
+
+        return teme;
+    }
+
+    private boolean TestAddAssignment(Tema assignment){
+        removeAllTeme();
+
+        boolean worked;
+        try{
+            service.addTema(assignment);
+            worked=true;
+        }catch (ValidationException ex){
+            worked=false;
+        }
+
+        return worked;
     }
 }
